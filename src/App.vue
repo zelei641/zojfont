4f2efcf682026603f64dab9941aa5841fdb94c63
2962006502@qq.com <2962006502@qq.com>
2024-04-05 10:32:24 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <a-calendar v-model="value" />
+  select: {{ value }}
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from "vue";
 
-nav {
-  padding: 30px;
-}
+export default {
+  setup() {
+    const value = ref(new Date("2023-01-01"));
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    return {
+      value,
+    };
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@arco-design/web-vue/dist/arco.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(ArcoVue).mount("#app");
