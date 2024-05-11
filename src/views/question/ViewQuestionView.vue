78843defaf0027e987711b5e2c4a14c27a6ace3e
2962006502@qq.com <2962006502@qq.com>
2024-05-11 19:54:43 +0800

    初版

--- src/views/question/ViewQuestionView.vue
+++ src/views/question/ViewQuestionView.vue
@@ -189,6 +189,24 @@ onMounted(() => {
   loadData();
 });
 
+/**
+ * 提交代码
+ */
+const doSubmit = async () => {
+  if (!question.value?.id) {
+    return;
+  }
+  const res = await QuestionSubmitControllerService.doQuestionSubmitUsingPost({
+    ...form.value,
+    questionId: question.value.id,
+  });
+  if (res.code === 0) {
+    message.success("提交成功");
+  } else {
+    message.error("提交失败," + res.message);
+  }
+};
+
 const onPageChange = async (page: number) => {
   searchParams.value = {
     ...searchParams.value,
